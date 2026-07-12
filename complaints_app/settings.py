e0b2fb39c4e48bfa74e42db75ed85a4b41d5a026
= <=>
2026-07-12 18:30:26 +0100

    configurations

--- complaints_app/settings.py
+++ complaints_app/settings.py
@@ -4,9 +4,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-change-this-in-production-abc123'
-DEBUG = False
 ALLOWED_HOSTS = ['kwanyise.pythonanywhere.com']
-
+DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
